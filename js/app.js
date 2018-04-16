0ab86e050c2e17fa1b1bdbe187bc8a9e179e7321
rodricksnyc <rodricksnyc@gmail.com>
2018-04-16 14:17:28 -0400

    added new info

--- js/app.js
+++ js/app.js
@@ -1,182 +1,150 @@
-$(document).ready(function() {
-
-var questions = ["1.What is Earth's largest continent?",
-"2.What river runs through Baghdad?",
-"3.What country has the most natural lakes?",
-"4.What is the driest place on Earth?",
-"5.What is the only sea without any coasts?",
-"6.Which African nation has the most pyramids?" ];
-
-var answers = [4,1,2,3,4,2,2];
-
-var answerInfo = ["Currently, Asia is Earth's largest continent at approximately 17,300,000 square miles (44,806,812 sq km). ",
-"he Tigris river runs through Baghdad. It is about 1,150 miles (1,800 km) long. The name 'Tigris' comes from Old Persian and translates as 'the fast one'.", "Canada has more than half of all the natural lakes in the world. An impressive nine percent of the country is covered in fresh water.","The driest place on Earth: the McMurdo Dry Valleys in Antactica. It hasn't rained there for more than 2 million years!", "The Sargasso Sea is a region in the middle of the North Atlantic Ocean.", "Sudan is home to over 200 pyramids, more than twice that of Egypt."]
-
-var answerPic = ["asia.png", "tigris.jpg", "canada.png",
-"mcmurdo.jpg", "sargassosea.png", "sudan.png"]
-
-var boxOneChoices = ["Europe", "Tigris","Australia", "Sahara Desert","Mediterranean Sea","Libya"];
-
-var boxTwoChoices = ["Antartica", "Jordan", "Canada", "Kufra, Libya", "Sargasso Sea","Sudan"];
-
-var boxThreeChoices = ["Africa","Euphrates","India", "McMurdo, Antartica", "Adriatic Sea","Egypt"];
-
-var boxFourChoices = ["Asia", "Karun", "United States", "Atacama, Desert", "Celebes Sea", "Algeria"];
-
-var i = 0;
-var guess = 1;
-var madeGuess = 0;
-var time = 1500;
-var timeBetween = 10;
-var wins = 0;
-var losses = 0;
-var timerExp = 0;
-var pointsEnabled = 0;
-var points = 0;
-var totalPointsEarned = 0;
-
-$(".begin").hide();
-$(".answerConfirmation").hide();
-$(".summary").hide();
-$(".answer").hide();
-$(".pointsArea").hide();
+$(document).ready(function () {
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+    var unansweredQuestions = 0;
+    var timeRemaining = 15;
+    var intervalID;
+    var indexQandA = 0;
+    var answered = false;
+    var correct;
+    var triviaGame = [{
+        question: "What is Earth's largest continent?",
+        answer: ["Europe","Antartica","Africa", "Asia"],
+        correct: "3",
+        image: ("img/asia.png")
+    }, {
+        question: "What river runs through Baghdad?",
+        answer: ["Tigris","Jordan", "Euphrates", "Karun"],
+        correct: "0",
+        image: ("img/tigris.jpg")
+    }, {
+        question: "What place has the most natural lakes?",
+        answer: ["Australia","Canada","India", "United States"],
+        correct: "1",
+        image: ("img/canada.png")
+    }, {
+        question: "Which African nation has the most pyramids?",
+        answer: ["Libya", "Sudan", "Egypt", "Algeria"],
+        correct: "1",
+        image: ("img/sudan.png")
+    }, {
+        question: "What is the driest place on Earth?",
+        answer: ["Sahara Desert", "Kufra, Libya", "McMurdo, Antartica", "Atacama, Desert"],
+        correct: "2",
+        image: ("img/mcmurdo.jpg")
+    }, {
+        question: "What is the only sea without any coasts?",
+        answer: ["Mediterranean Sea", "Sargasso Sea", "Adriatic Sea", "Celebes Sea"],
+        correct: "1",
+        image: ("img/sargassosea.png")
+    }];
 
 
-$("#geography").click(function() {
-	$("body").css("background-image","url('images/background.jpg')");
-	$(".begin").fadeIn();
-	$(".chooseGame")
-	$(".summary").hide();
-	$(".answer").hide();
-	$("#timer").fadeIn();
-	$("#pointsMode").hide();
-	i = 20;
-	nextQuestion();
-});
+    function startGame() {
+        console.log("game has begun");
+        $('.start-button').remove();
+        correctAnswers = 0;
+        incorrectAnswers = 0;
+        unansweredQuestions = 0;
+        loadQandA();
+    }
 
+    function loadQandA() {
+        answered = false;
+        timeRemaining = 16;
+        intervalID = setInterval(timer, 1000);
+        if (answered === false) {
+            timer();
+        }
+        correct = triviaGame[indexQandA].correct;
+        var question = triviaGame[indexQandA].question;
+        $('.question').html(question);
+        for (var i = 0; i < 4; i++) {
+            var answer = triviaGame[indexQandA].answer[i];
+            $('.answers').append('<h4 class= answersAll id=' + i + '>' + answer + '</h4>');
+        }
 
-$("#pointsMode").click(function() {
-	if (pointsEnabled === 0) {
-		pointsEnabled = 1;
-		$(".pointsArea").fadeIn("slow");
-		 }
-	else { pointsEnabled = 0;
-		$(".pointsArea").fadeOut("slow"); }
-});
+        $("h4").click(function () {
+            var id = $(this).attr('id');
 
-
-$("#boxOne").click(function() {
-	$("#timer").html("");
-	if (madeGuess === 0) {
-		guess = 1;
-		madeGuess = 1;
-		checkAnswer(); }
-});
-
-$("#boxTwo").click(function() {
-	$("#timer").html("");
-	if (madeGuess === 0) {
-		guess = 2;
-		madeGuess = 1;
-		checkAnswer(); }
-});
-
-
-$("#boxThree").click(function() {
-	$("#timer").html("");
-	if (madeGuess === 0) {
-		guess = 3;
-		madeGuess = 1;
-		checkAnswer(); }
-});
-
-$("#boxFour").click(function() {
-	$("#timer").html("");
-	if (madeGuess === 0) {
-		guess = 4;
-		madeGuess = 1;
-		checkAnswer(); }
-});
-
-
-
-function nextQuestion() {
-
-	if (wins + losses + unanswered === 10) { gameOver(); }
-	else {	madeGuess = 0;
-
-			time = 1500;
-			timeBetween = 7;
-			$("#pointsEarned").hide();
-			$(".answerConfirmation")
-			$(".btn-info").hide();
-			$(".btn-info").css("background-color", "pink");
-			$(".btn-info").fadeIn();
-			$("#questionArea").text(questions[i]);
-			$("#boxOne").text(boxOneChoices[i]);
-			$("#boxTwo").text(boxTwoChoices[i]);
-			$("#boxThree").text(boxThreeChoices[i]);
-			$("#boxFour").text(boxFourChoices[i]);
-			$("#inBetween").html("");
-			timer(); }
+            if (id === correct) {
+                answered = true;
+                $('.question').text("The answer is: " + triviaGame[indexQandA].answer[correct]);
+                correctAnswer();
+            } else {
+                answered = true;
+                $('.question').text("You chose: " + triviaGame[indexQandA].answer[id] + "but the correct answer is: " + triviaGame[indexQandA].answer[correct]);
+                incorrectAnswer();
+            }
+        });
 }
 
+    function timer() {
+        if (timeRemaining === 0) {
+            answered = true;
+            clearInterval(intervalID);
+            $('.question').text("The correct answer is: " + triviaGame[indexQandA].answer[correct]);
+            unAnswered();
+        } else if (answered === true) {
+            clearInterval(intervalID);
+        } else {
+            timeRemaining--;
+            $('.timeRemaining').text('You have ' + timeRemaining + ' seconds to choose');
+        }
+    }
 
-function timer () {
-	intervalId = setInterval(count, 20);
-}
+    function correctAnswer() {
+        correctAnswers++;
+        $('.timeRemaining').text("You answered correctly").css({
+            'color': '#3D414F'
+        });
+        resetRound();
+    }
 
+    function incorrectAnswer() {
+        incorrectAnswers++;
+        $('.timeRemaining').text("You answered incorrectly!").css({
+            'color': '#3D414F'
+        });
+        resetRound();
 
-function count () {
-	if (time === 0) { clearInterval(intervalId);
-					  unanswered++;
-					  checkAnswer(); }
-	else if (time % 100 === 0) { $("#timer").html(time/200); }
-	time--;
-}
+    }
 
-function checkAnswer() {
-	clearInterval(intervalId);
-	if (pointsEnabled === 0) { $("#pointsEarned").hide(); }
-	else { $("#pointsEarned").show(); }
-	if (guess === answers[i])
-								$("#pointsEarned").css("color", "greenyellow");
-								$("#pointsEarned").html("You earned " + time + " points for that question.");
-								totalPointsEarned = totalPointsEarned + time;
-								wins++; }
+    function unAnswered() {
+        unansweredQuestions++;
+        $('.timeRemaining').text("You didn't answe the question").css({
+            'color': '#3D414F'
+        });
+        resetRound();
+    }
 
+    function resetRound() {
+        $('.answersAll').remove();
+        $('.answers').append('<img class=answerImage width="150" height="150" src="' + triviaGame[indexQandA].image + ' ">');
+        indexQandA++;
+        if (indexQandA < triviaGame.length) {
+            setTimeout(function () {
+                loadQandA();
+                $('.answerImage').remove();
+            }, 5000);
+        } else {
+            setTimeout(function () {
+                $('.question').remove();
+                $('.timeRemaining').remove();
+                $('.answerImage').remove();
+                $('.answers').append('<h4 class= answersAll end>Correct Answers: ' + correctAnswers + '</h4>');
+                $('.answers').append('<h4 class= answersAll end>Incorrect Answers: ' + incorrectAnswers + '</h4>');
+                $('.answers').append('<h4 class= answersAll end>Unanswered Questions: ' + unansweredQuestions + '</h4>');
+                setTimeout(function () {
+                    location.reload();
+                }, 7000);
+            }, 5000);
+        }
+    };
 
-			$("#pointsEarned").css("color", "red");
-			$("#pointsEarned").html("You lost -" + time + " points for that question.");
-			totalPointsEarned = totalPointsEarned - time;
-
-			if (timerExp === 0) { losses++; }
-			timerExp = 0;
-
-
-function gameOver() {
-	$("#timer").fadeOut("slow");
-	$(".summary").fadeIn("slow");
-	$(".begin").fadeOut("slow");
-	$(".answerConfirmation").fadeOut("slow");
-	clearInterval(intervalId);
-	$("#wins").html(wins);
-	$("#losses").html(losses);
-	$("#unanswered").html(unanswered);
-	$(".chooseGame").fadeIn("slow");
-	$("#pointsMode").fadeIn("slow");
-	$("#totalPointsEarned").html(totalPointsEarned);
-	$(".pointsArea").hide();
-	$("#pointsEarned").html("");
-	$("#inBetween").html("");
-	wins = 0;
-	losses = 0;
-	unanswered = 0;
-	guess = 0;
-	madeGuess = 0;
-	i = 0;
-	totalPointsEarned = 0;
-	pointsEnabled = 0;
-}
+    $('.startButton').on("click", function () {
+        $('.startButton');
+        startGame();
+    });
 
 });
